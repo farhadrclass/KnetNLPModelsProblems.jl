7767d2291ef8d70bd8f88b89e36280cc678b2856
nathanemac <91251698+nathanemac@users.noreply.github.com>
2022-07-25 12:59:27 -0400

    add verbose, fix epoch_all!

--- examples/LeNet_MNIST.jl
+++ examples/LeNet_MNIST.jl
@@ -4,17 +4,19 @@ include("..//src//Lenet_mnist.jl")
 include("..//src//FC_mnist.jl")
 
 
+T = Float16
 
 (xtrn, ytrn), (xtst, ytst) = loaddata(1)
-dtrn = minibatch(xtrn, ytrn, 100; xsize = (size(xtrn, 1), size(xtrn, 2), 1, :))
-dtst = minibatch(xtst, ytst, 100; xsize = (size(xtst, 1), size(xtst, 2), 1, :))
+dtrn = minibatch(xtrn, ytrn, 100; xtype = T, ytype = T, xsize = (size(xtrn, 1), size(xtrn, 2), 1, :))
+dtst = minibatch(xtst, ytst, 100; xtype = T, ytype = T, xsize = (size(xtst, 1), size(xtst, 2), 1, :))
+
 
 # size of minibatch 
 m = 100
 
-
 knetModel, myModel = lenet_prob(xtrn, ytrn, xtst, ytst, minibatchSize = m)
 
+trained_model = train_knetNLPmodel!(myModel, R2, xtrn, ytrn; mbatch = m, mepoch = 15, maxTime = 100, all_data = true, verbose = false)
 
 
 
@@ -31,3 +33,8 @@ knetModel, myModel = lenet_prob(xtrn, ytrn, xtst, ytst, minibatchSize = m)
 # end
 
 
+res = trained_model[2]
+epochs = res[:, 1]
+acc = res[:, 2]
+
+fig = plot(epochs, title="Best accuracy vs Epoch on Float16", acc, label="best accuracy", legend=:bottomright, xlabel = "epoch", ylabel = "accuracy")
--- src/utils.jl
+++ src/utils.jl
@@ -18,8 +18,8 @@ using LinearAlgebra
 include("struct_utils.jl")
 function loaddata(data_flag)
     if (data_flag == 1)
-        @info("Loading MNITS...")
-        xtrn, ytrn = MNIST.traindata(Float32)
+        @info("Loading MNIST...")
+        xtrn, ytrn = MNIST.traindata(T)
         ytrn[ytrn.==0] .= 10
         xtst, ytst = MNIST.testdata(Float32)
         ytst[ytst.==0] .= 10
@@ -49,10 +49,10 @@ uses the whole test data sets"""
 All_accuracy(nlp::AbstractKnetNLPModel) = Knet.accuracy(nlp.chain; data = nlp.data_test)
 
 #runs over only one random one
-function epoch!(modelNLP, solver, xtrn, ytrn; mbatch = 64)
-    reset_minibatch_train!(LeNetNLPModel)
-    @info("Minibatch = ", i)
-    stats = solver(modelNLP)
+function epoch!(modelNLP, solver, epoch, verbose; mbatch = 64)
+    reset_minibatch_train!(modelNLP)
+    @info("Epoch # ", epoch)
+    stats = solver(modelNLP; verbose = verbose)
     new_w = stats.solution
     set_vars!(modelNLP, new_w)
     @info("Minibatch Accracy: ", KnetNLPModels.accuracy(modelNLP))
@@ -60,18 +60,18 @@ function epoch!(modelNLP, solver, xtrn, ytrn; mbatch = 64)
 end
 
 #run over all minibatched 
-function epoch_all!(modelNLP, solver, xtrn, ytrn; mbatch = 64)
+function epoch_all!(modelNLP, solver, epoch, verbose; mbatch = 64)
     #training loop to go over all the dataset
+    @info("Epoch # ", epoch)
     for i = 0:(length(ytrn)/m)-1
-        reset_minibatch_train!(LeNetNLPModel)
+        reset_minibatch_train!(modelNLP)
         @info("Minibatch = ", i)
-        stats = solver(modelNLP)
+        stats = solver(modelNLP; verbose = verbose)
         new_w = stats.solution
         set_vars!(modelNLP, new_w)
         @info("Minibatch Accracy: ", KnetNLPModels.accuracy(modelNLP))
     end
     return KnetNLPModels.accuracy(modelNLP)
-
 end
 
 function train_knetNLPmodel!(
@@ -83,15 +83,16 @@ function train_knetNLPmodel!(
     mepoch = 10,
     maxTime = 100,
     all_data = false,
+    verbose = true
 )
 
     acc_arr = []#todo fix this 
     best_acc = 0
     for j = 0:mepoch
         if all_data
-            acc = epoch_all!(modelNLP, solver, xtrn, ytrn; mbatch = 64)
+            acc = epoch_all!(modelNLP, solver, j, verbose; mbatch)
         else
-            acc = epoch!(modelNLP, solver, xtrn, ytrn; mbatch = 64)
+            acc = epoch!(modelNLP, solver, j, verbose; mbatch)
         end
         if j % 10 == 0
             @info("epoch #", j, "  acc= ", acc)
